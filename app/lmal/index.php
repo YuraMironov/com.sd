1fab0ccfb65d77a160e228434b41234a981dc1f6
Yuriy Mironov <jura96_@mail.ru>
2017-11-27 20:37:00 +0300

    add LMAL presentation file

--- app/lmal/index.php
+++ app/lmal/index.php
@@ -1,3 +1,19 @@
 <?php
+require_once ("LMAL.php");
+$input1 = array(1,2,3,4,3,6,7,8,9,356,4,3,2,1,0);
+$input2 = array(9,8,7,9,5,4,3,2,1,0);
 
-echo 'hi';
+function test($input)
+{
+    print_r($input);
+    echo "<br> get local max <br>";
+    print_r(LMAL::getLocalMax($input));
+    echo "<br> get max length <br>";
+    print_r(LMAL::getMaxLength($input));
+    echo "<br>";
+    echo "<br>";
+}
+
+test($input1);
+test($input2);
+
