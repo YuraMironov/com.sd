c72970518c6c506ea63a84e7b5bc0cfd60c9a15b
Yuriy Mironov <jura96_@mail.ru>
2017-11-27 21:55:41 +0300

    add ListInterface

---
+++ app/mc/ListInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Yura
+ * Date: 27.11.2017
+ * Time: 21:51
+ */
+require_once ('AbstractProduct.php');
+
+interface ListInterface extends Countable
+{
+    public function get(int $index):AbstractProduct;
+    public function add(AbstractProduct $product): bool;
+    public function insert(int $index, AbstractProduct $product): bool;
+    public function remove(int $index): bool;
+
+}
