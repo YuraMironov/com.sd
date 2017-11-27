cb4a42fb68a5c7a50514e432e1d7d9312e41fa0a
Yuriy Mironov <jura96_@mail.ru>
2017-11-27 13:50:30 +0300

    create tests for LMAL

--- test/LMALTest.php
+++ test/LMALTest.php
@@ -6,8 +6,44 @@
  * Date: 26.11.2017
  * Time: 18:38
  */
+require_once ('../app/lmal/LMAL.php');
 use PHPUnit\Framework\TestCase;
 class LMALTest extends TestCase
 {
+    public function provideLocalMax()
+    {
+        return array(
+            array(array(1,2,3,4,5,6,4,7,1), array(6,7)),
+            array(array(1,2,3,4,5,6,7,7,9), array()),
+            array(array(1,2), false)
+        );
+    }
+
+    /**
+     * @dataProvider provideLocalMax
+     */
+    public function testLocalMax($input, $expected)
+	{
+        $result = LMAL::getLocalMax($input);
+        $this->assertSame($expected, $result);
+        unset($result);
+	}
+	public function provideMaxLength()
+    {
+        return array(
+            array(array(1,2,3,4,5,6,7), 0),
+            array(array(9,7,6,9,5,4,3,2,1), 6)
+        );
+    }
+
+    /**
+     * @dataProvider provideMaxLength
+     */
+	public function testMaxLength($input, $expected)
+	{
+	    $result = LMAL::getMaxLength($input);
+        $this->assertSame($expected, $result);
+        unset($result);
+	}
 
 }
