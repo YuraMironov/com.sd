41142782c64cb55021fb84518a7c35978da6c1c9
Yuriy Mironov <jura96_@mail.ru>
2017-11-30 22:53:48 +0300

    edit MyQueue

--- app/queuetask/MyQueue.php
+++ app/queuetask/MyQueue.php
@@ -2,73 +2,74 @@
 
 
 class MyQueue
-{	
-	private $maxLength = 10;
-	private $length = 0;
-	private $queue = null;
-	private $first = null;
-	private $last = null;
-	public function __construct(array $args = array())
-	{	
-		$this->queue = array();
-		if (count($args) != 0) {
-			$i = 0;
-			foreach ($args as $value) {
-				if ($i < $this->maxLength) {
-					array_push($this->queue, $value);
-					$i++;
-				}
-			}
-		}
-	}
-	public function getQueue()
-	{
-		return $this->queue;
-	}
-	public function getBusy()
-	{
-		return count($this->queue);
-	}
+{
+    private $maxLength = 10;
+    private $queue = null;
 
-	public function addToEnd($value) 
-	{	
-		if (!(is_array($value)) && $this->getBusy() < 10) {
-			array_push($this->queue, $value);
-			return true;
-		}
-		echo "queue is busy";
-		return false;
-	}
-	public function getFirstKey()
-	{
-		if ($this->getBusy() > 0){
-			return array_keys($this->queue)[0];
-		}
-		return false;
-	}
-	public function deleteFirst()
-	{
-		if ($this->getBusy() > 0){
-			unset($this->queue[$this->getFirstKey()]);
-			return true;
-		} else {
-			echo "Queue is empty! \n";
-		}
-		return false;
-	}
-	public function getFirstAndLast()
-	{
-		if ($this->getBusy() > 0) {
-			$first = $this->queue[$this->getFirstKey()];
-			$last = array_pop($this->queue);
-			array_push($this->queue, $last);
-			$firstAndLast = [
-				'first' => $first,
-				'last' =>$last
-			];
-			return $firstAndLast;
-		}
-		return false;
-	}
+    public function __construct(array $args = array())
+    {
+        $this->queue = array();
+        if (count($args) != 0) {
+            foreach (array_slice($args, 0, $this->maxLength) as $value) {
+                $this->addToEnd($value);
+            }
+        }
+    }
+
+    public function getQueue(): array
+    {
+        return $this->queue;
+    }
+
+    public function getBusy(): int
+    {
+        return count($this->queue);
+    }
+
+    public function addToEnd(array $value): bool
+    {
+        if ($this->getBusy() < $this->maxLength) {
+            $this->queue[] = $value;
+            return true;
+        }
+        return false;
+    }
+
+    public function getFirstKey(): ?int
+    {
+        if ($this->getBusy() > 0) {
+            return array_keys($this->queue)[0];
+        }
+        return null;
+    }
+    public function getLastKey(): ?int
+    {
+        if ($this->getBusy() > 0) {
+            return array_pop(array_keys($this->queue));
+        }
+        return null;
+    }
+
+    public function deleteFirst(): bool
+    {
+        if ($this->getBusy() > 0) {
+            unset($this->queue[$this->getFirstKey()]);
+            return true;
+        }
+        return false;
+    }
+
+    public function getFirstAndLast(): ?array
+    {
+        if ($this->getBusy() > 0) {
+            $first = $this->queue[$this->getFirstKey()];
+            $last = $this->queue[$this->getLastKey()];
+            return [
+                'first' => $first,
+                'last' => $last
+            ];
+        }
+        return null;
+    }
 
 }
